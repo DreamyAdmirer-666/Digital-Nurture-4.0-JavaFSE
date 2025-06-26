2065255fc43d4797d4eb3a03d53c7b257ea8e969
DreamyAdmirer-666 <121563018+DreamyAdmirer-666@users.noreply.github.com>
2025-06-27 01:50:35 +0530

    Delete Week 2/PLSQL_Programming/TDD using JUnit5 and Mockito directory

--- Week 2/PLSQL_Programming/TDD using JUnit5 and Mockito/Exercise 1/Calculator.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
--- Week 2/PLSQL_Programming/TDD using JUnit5 and Mockito/Exercise 4/Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public void close() {
-        System.out.println("Cleaning up Calculator");
-    }
-}
