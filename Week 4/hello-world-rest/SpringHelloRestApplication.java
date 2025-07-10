4d1c6f568b4cd0a2bff80608422d9511d038d21a
DreamyAdmirer-666 <121563018+DreamyAdmirer-666@users.noreply.github.com>
2025-07-11 01:05:18 +0530

    Add files via upload

---
+++ Week 4/hello-world-rest/SpringHelloRestApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloRestApplication{
+    public static void main(String[] args){
+        SpringApplication.run(SpringHelloRestApplication.class,args);
+    }
+}
